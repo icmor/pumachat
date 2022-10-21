73987207ea5fd27f2959d4876d2238548bf9e8ac
icmor <cornejodlm@ciencias.unam.mx>
2022-10-21 16:04:35 -0500

    Client login

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from utils import BaseChat
+from utils import BaseChat, MessageException
 from message import Message
 import asyncio
 import socket
@@ -19,17 +19,47 @@ class Client(BaseChat):
             asyncio.open_connection(self.host, self.port)
 
     async def run(self):
-        await self.send(
-            Message({"type": "IDENTIFY", "username": "jane_doe"})
-        )
-        return await self.recv()
+        try:
+            while True:
+                await self.connect()
+                if (not await self.login()
+                        and input("Reintentar login? [y/n] ").lower()
+                        in ["yes", "y"]):
+                    continue
+                break
+        except (OSError, ConnectionRefusedError) as e:
+            print("Error de conexión.")
+            logging.debug(str(e))
+        except MessageException as e:
+            logging.debug(str(e))
+        finally:
+            await self.cleanup()
+
+    async def login(self):
+        username = input("Enter your username: ")
+        await self.send(Message({"type": "IDENTIFY",
+                                 "username": username}))
+        response = await self.recv()
+        logging.debug(response.__repr__())
+        match response:
+            case {"type": "INFO", "message": "success"}:
+                print(f"Login exitoso! Bienvenido {username}")
+                return True
+            case {"type": "WARNING"}:
+                print(response["message"])
+                return False
+            case _:
+                logging.error("Expected message type 'WARNING'. Got",
+                              response.__repr__())
+                raise MessageException("Unrecognized response from server")
+
+    async def cleanup(self):
+        pass
 
 
 async def main():
     client = Client()
-    await client.connect()
-    print(await client.run())
-
+    await client.run()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING,
